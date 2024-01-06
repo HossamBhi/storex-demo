d867cd54e183fccf6c8d65884e464269f1712cc5
Hossam Bhi Pasha <hossambasha9900@gmail.com>
2024-01-06 17:07:05 +0200

    Init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { SimpleForm } from "storex-demo-package";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleForm
+        onSubmit={(e, values) => console.log({ values })}
+        getApiData={(data) =>
+          console.log("Api Data from storex-demo-package: ", data)
+        }
+      />
     </div>
   );
 }
